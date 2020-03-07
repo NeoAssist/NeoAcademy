6ece9567dc3017e78d6b49db1492ec1c5b9ae37b
Thales de França <ths.france@gmail.com>
2020-03-07 01:30:18 -0300

    code review

--- server/cmd/server/server.go
+++ server/cmd/server/server.go
@@ -12,7 +12,6 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
 
-
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
