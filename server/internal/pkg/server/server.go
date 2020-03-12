c9841aecfea794ca9ba10136beccc8bae0348e13
Thales de França <ths.france@gmail.com>
2020-03-12 13:38:41 -0300

    added go mod

--- server/internal/pkg/server/server.go
+++ server/internal/pkg/server/server.go
@@ -12,7 +12,7 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
 
-func main() {
+func RunServer() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	log.Fatal(http.ListenAndServe(":8080", router))
