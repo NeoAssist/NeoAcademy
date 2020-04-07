4247091b6fcd2e63e2f6e66838b80448fa480fdd
Gabriel Antiqueira <gantiqueira@neoassist.com>
2020-04-07 16:22:03 -0300

    WIP | Migrating frontend to Next.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,24 +1,54 @@
 module.exports = {
-  root: true,
   env: {
-    browser: true,
-    node: true
+      es6: true,
+      jest: true,
+      browser: true,
   },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+      __DEV__: true,
+  },
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+      ecmaFeatures: {
+          jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
   },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
+  plugins: ['react', 'jsx-a11y', 'import', 'react-hooks', 'prettier'],
   rules: {
-    'nuxt/no-cjs-in-config': 'off'
-  }
-}
+      'prettier/prettier': 'error',
+      'react/jsx-filename-extension': [
+          'error',
+          { extensions: ['.js', '.jsx'] },
+      ],
+      'import/prefer-default-export': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'react/jsx-one-expression-per-line': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'global-require': 'off',
+      'react-native/no-raw-text': 'off',
+      'no-param-reassign': 'off',
+      'no-underscore-dangle': 'off',
+      camelcase: 'off',
+      'no-console': ['error', { allow: ['tron'] }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'jsx-a11y/label-has-associated-control': [
+          {
+              controlComponents: ['Input'],
+          },
+      ],
+  },
+  settings: {
+      'import/resolver': {
+          'babel-plugin-root-import': {
+              rootPathSuffix: 'src',
+              rootPathPrefix: '@/',
+          },
+      },
+  },
+};
